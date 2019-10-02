0bdb759ce847bbdc39bac8a1db4c2441a707e812
juiceJosh <joshua_paul_manalili@dlsu.edu.ph>
2019-10-02 21:47:17 +0800

    Implement bootstrap, loginscreen and display screen

---
+++ src/container/DisplayScreen/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './styles.scss'
+
+export class index extends Component {
+    render() {
+        return (
+            <div>
+                Display Screen
+            </div>
+        )
+    }
+}
+
+export default index
---
+++ src/container/LoginScreen/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './styles.scss'
+
+
+export class index extends Component {
+    render() {
+        return (
+            <div className="container">
+                Login Page
+            </div>
+        )
+    }
+}
+
+export default index
